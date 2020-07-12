bdda8bedfaf3c31d9697b38cd97fef2ecce18702
Sergey <shapa92sergey@gmail.com>
2020-07-12 23:53:49 +0300

    made a timer

--- index.js
+++ index.js
@@ -5,22 +5,41 @@ const refs = {
   secs: document.querySelector('span[data-value="secs"]'),
 };
 
-console.dir(refs.days);
+// const currentTime = Date.now();
+// console.log(currentTime);
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-// function pad(value) {
-//   return String(value).padStart(2, '0');
-// }
+function updateClockface(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-// function updateClockface(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
 
-//   const hours = pad(
-//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-//   );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
 
-//   //   refs.days.textContent = `${hours}:${mins}:${secs}`;
-// }
+function reverseTimer() {
+  let currentTime = null;
+  let time = null;
+  const promotionEndDate = new Date('Jul 30, 2020');
+  const targetTime = promotionEndDate.getTime();
+  const timerId = setInterval(() => {
+    currentTime = Date.now();
+    time = targetTime - currentTime;
+    updateClockface(time);
+  }, 1000);
+
+  return time;
+}
+
+reverseTimer();
